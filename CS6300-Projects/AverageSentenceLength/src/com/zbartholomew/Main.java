d06f0be7a19da2a6e1577bd70e5ff8bfa8470022
Zach Bartholomew <zach.bartholomew@gmail.com>
2017-08-03 15:40:22 -0700

    Add command line argument checks.  Still need error checking

---
+++ CS6300-Projects/AverageSentenceLength/src/com/zbartholomew/Main.java
@@ -0,0 +1,52 @@
+package com.zbartholomew;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filepath = "", delimiters = "";
+        int wordLengthLimit = 0;
+
+        // check if command line arguments are present
+        if (args.length > 0) {
+            for (int i = 0; i < args.length - 1; i++) {
+                String token = args[i];
+
+                if (token.startsWith("-d")) {
+                    delimiters = args[i + 1];
+                    i++;
+
+                } else if (token.startsWith("w/-l")) {
+                    wordLengthLimit = Integer.parseInt(args[i + 1]);
+                    i++;
+
+                } else if (token.startsWith("/")) {
+                    filepath = token;
+                }
+            }
+
+            System.out.println("filepath: " + filepath + "\n" +
+                    "delimiters: " + delimiters + "\n" +
+                    "word length limit: " + wordLengthLimit);
+        } else {
+            // Prompt user for required fields if not supplied
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Please enter filepath of essay: ");
+            filepath = in.next();
+
+            System.out.println("Please enter delimiters that identify a sentence: ");
+            delimiters = in.next();
+
+            System.out.println("Please enter word length limit of a sentence that should be considered: ");
+            wordLengthLimit = in.nextInt();
+            in.close();
+
+            System.out.println("filepath: " + filepath + "\n" +
+                    "delimiters: " + delimiters + "\n" +
+                    "word length limit: " + wordLengthLimit);
+        }
+    }
+}
